707044e1c145bc4474742c3d412e2755832e1284
juanitotaveras <juanito.taveras@gmail.com>
2016-08-27 14:37:20 -0700

    2nd commit

---
+++ sites/default/files/php/twig/48b3c177_input--button--dropdown.html.twig_887fb082daa7c2f372f855d9edca60aa9399fb1d6a6e12f8993695bf6eca4336/fd772a2318a3293eadfcd7192f04b4113c3be936edf37aaadb1825875b98aa07.php
@@ -0,0 +1,121 @@
+<?php
+
+/* themes/bootstrap/templates/input/input--button--dropdown.html.twig */
+class __TwigTemplate_33126ff49042dc16343279c33c11c4ef96bf8e214fbbe79c9844bc27abffb1e9 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("input--button.html.twig", "themes/bootstrap/templates/input/input--button--dropdown.html.twig", 1);
+        $this->blocks = array(
+            'input' => array($this, 'block_input'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "input--button.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("spaceless" => 25);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array('spaceless'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 25
+        ob_start();
+        echo trim(preg_replace('/>\s+</', '><', ob_get_clean()));
+        // line 1
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 26
+    public function block_input($context, array $blocks = array())
+    {
+        // line 27
+        echo "    <a href=\"#\" data-target=\"dropdown-button\">";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["label"]) ? $context["label"] : null), "html", null, true));
+        echo "</a>
+    <button";
+        // line 28
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["attributes"]) ? $context["attributes"] : null), "addClass", array(0 => "hidden"), "method"), "html", null, true));
+        echo ">";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["label"]) ? $context["label"] : null), "html", null, true));
+        echo "</button>
+    ";
+        // line 29
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["children"]) ? $context["children"] : null), "html", null, true));
+        echo "
+  ";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/bootstrap/templates/input/input--button--dropdown.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  70 => 29,  64 => 28,  59 => 27,  56 => 26,  52 => 1,  49 => 25,  11 => 1,);
+    }
+}
+/* {% extends "input--button.html.twig" %}*/
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Theme suggestion for "button__dropdown" input form element.*/
+/*  **/
+/*  * Available variables:*/
+/*  * - attributes: A list of HTML attributes for the input element.*/
+/*  * - children: Optional additional rendered elements.*/
+/*  * - icon: An icon.*/
+/*  * - icon_only: Flag to display only the icon and not the label.*/
+/*  * - icon_position: Where an icon should be displayed.*/
+/*  * - label: button label.*/
+/*  * - prefix: Markup to display before the input element.*/
+/*  * - suffix: Markup to display after the input element.*/
+/*  * - type: The type of input.*/
+/*  **/
+/*  * @ingroup templates*/
+/*  **/
+/*  * @see \Drupal\bootstrap\Plugin\Preprocess\InputButton*/
+/*  * @see \Drupal\bootstrap\Plugin\Preprocess\Input*/
+/*  * @see template_preprocess_input()*/
+/*  *//* */
+/* #}*/
+/* {% spaceless %}*/
+/*   {% block input %}*/
+/*     <a href="#" data-target="dropdown-button">{{ label }}</a>*/
+/*     <button{{ attributes.addClass('hidden') }}>{{ label }}</button>*/
+/*     {{ children }}*/
+/*   {% endblock %}*/
+/* {% endspaceless %}*/
+/* */
