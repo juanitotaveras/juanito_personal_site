94623cae44f6125e556d2e3af369f2a42f59529f
juanitotaveras <juanito.taveras@gmail.com>
2016-08-27 14:20:27 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# juanito_personal_site
---
+++ sites/default/files/php/twig/48b3c177_form--search-block-form.html.twig_8e819b2aa7a8169f20ed12b4cba27e2d887055d0b8d1bf1e6e19019c02055fe1/d3a1048f4e6f05ff25b9a30cc3fbc36d0de7ba40dfae409b62cdb0300310ff22.php
@@ -0,0 +1,84 @@
+<?php
+
+/* core/themes/bartik/templates/form--search-block-form.html.twig */
+class __TwigTemplate_3ff6478e2df6d4d2d3351e3ac42988f1b8d60aac97e9f38b054a9895c090d6b9 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 13
+        echo "<form";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["attributes"]) ? $context["attributes"] : null), "addClass", array(0 => "search-form", 1 => "search-block-form"), "method"), "html", null, true));
+        echo ">
+  ";
+        // line 14
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["children"]) ? $context["children"] : null), "html", null, true));
+        echo "
+</form>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/bartik/templates/form--search-block-form.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  48 => 14,  43 => 13,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Default theme implementation for a 'form' element.*/
+/*  **/
+/*  * Available variables:*/
+/*  * - attributes: A list of HTML attributes for the wrapper element.*/
+/*  * - children: The child elements of the form.*/
+/*  **/
+/*  * @see template_preprocess_form()*/
+/*  *//* */
+/* #}*/
+/* <form{{ attributes.addClass('search-form', 'search-block-form') }}>*/
+/*   {{ children }}*/
+/* </form>*/
+/* */
